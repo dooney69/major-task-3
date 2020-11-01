2621a2e6fbd10ce180fea9554a0969bb3a69a5dd
dooney69 <65260475+dooney69@users.noreply.github.com>
2020-11-01 12:21:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 function solve(){
-    var hours = parseFloat(document.getElementById('hoursWorked').value);
-    var extraHour = parseFloat(document.getElementById('extraHour').value);
+    var hours = document.getElementById('hoursWorked').value;
+    var extraHour = document.getElementById('extraHour').value;
+  var extra = extraHour * 1200
     
     if (hours <= 4){
         wage = (hours * 1000);
-        document.getElementById('final').innerHTML = "Your total wage is" + wage + "Naira only.";
-    }else{
-        wage = (hours * 1000) ;
-        extra = (extraHour * 1200);
+    } 
+  else{
+        wage = (hours * 1000) 
+        extra = (extraHour * 1200)
         newWage = (wage + extra);
-        document.getElementById('final').innerHTML = `Your total wage is${newWage}Naira only.`;
     }
+   
+  console.log(wage);
+  console.log(newWage = wage + extra);
+   document.getElementById('final').innerHTML = ` Your wage is ${newWage} Naira only `;
 }
-
-
-var submit = document.getElementById('submit');
-submit.onclick = function (){
+var submitButton = document.getElementById('submit');
+submitButton.onclick = function (){
     solve();
 }
+
+ 
