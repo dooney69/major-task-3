56b6cf1da9201c720ee90dbf61b6b3200011cd22
dooney69 <65260475+dooney69@users.noreply.github.com>
2020-11-01 10:15:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ function solve(){
     
     if (hours <= 4){
         wage = (hours * 1000);
+        document.getElementById('final').innerHTML = "Your total wage is" + wage + "Naira only.";
     }else{
-        wage = (hours * 1000) 
-        extra = (extraHour * 200)
+        wage = (hours * 1000) ;
+        extra = (extraHour * 1200);
         newWage = (wage + extra);
+        document.getElementById('final').innerHTML = `Your total wage is${newWage}Naira only.`;
     }
 }
 
